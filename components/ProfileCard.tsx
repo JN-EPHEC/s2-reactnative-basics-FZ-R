23ce323e109396bdc7cf24c6279e325a8312c468
FZ-R <framhani@gmail.com>
2025-10-04 21:55:03 +0200

    Ex1 : ProfileCardComponent

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 // Define the props interface for type safety
 interface ProfileCardProps {
@@ -10,47 +10,55 @@ interface ProfileCardProps {
 
 // The component receives 'props' as an argument.
 // We use destructuring to get the values we need directly.
-const ProfileCard: React.FC<ProfileCardProps> = ({
-  name,
-  jobTitle,
-  imageUrl,
-}) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({name, jobTitle,imageUrl,}) => {
   return (
-    <>
-      <Text>Replace this part with your soluce</Text>
-    </>
+    <View style={styles.card}>
+
+      <Image style={styles.image} source={{uri:imageUrl}}/>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.jobTitle}>{jobTitle}</Text> 
+      
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "#fff",
+    backgroundColor: "#b5d1e9e4",
     borderRadius: 16,
-    padding: 20,
+    padding: 10,
     margin: 16,
     flexDirection: "row",
     alignItems: "center",
-    shadowColor: "#000",
+    shadowColor: "#811d1dff",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 5,
+   
   },
   image: {
     width: 80,
     height: 80,
     borderRadius: 40,
+    marginRight : 10,
+    borderWidth: 1,
+    borderColor : "black",
   },
   textContainer: {
-    marginLeft: 16,
+    marginLeft: 26,
+    
   },
   name: {
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 30,
+    fontWeight: 700,
+    
   },
   jobTitle: {
     fontSize: 16,
-    color: "gray",
+    color: "bleu",
+    marginLeft: 'auto',
+
   },
 });
 
